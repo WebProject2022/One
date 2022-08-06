7fe00c7bd9b5914f334aeb213051a235c2fb27c7
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-08-06 17:57:14 -0400

    DC

--- js/my-app.js
+++ js/my-app.js
@@ -4,21 +4,39 @@ if (screen.width < 574) {
     ScrollReveal().reveal('.card', {
         duration: 1500,
         origin: 'bottom',
-        distance: '0px'
-        
+        distance: '40px'
+
+    });
+    ScrollReveal().reveal('.card-animation', {
+        duration: 1500,
+        origin: 'right',
+        distance: '40px'
+
+    });
+    ScrollReveal().reveal('.h1', {
+        duration: 1500,
+        origin: 'top',
+        distance: '40px'
+
     });
     ScrollReveal().reveal('.h3', {
-    duration: 1500,
-    origin: 'left',
-    distance: '40px'
-    
-});
-ScrollReveal().reveal('.p1', {
-    duration: 1500,
-    origin: 'right',
-    distance: '40px'
-    
-});
+        duration: 1500,
+        origin: 'left',
+        distance: '40px'
+
+    });
+    ScrollReveal().reveal('.h5', {
+        duration: 1500,
+        origin: 'right',
+        distance: '40px'
+
+    });
+    ScrollReveal().reveal('.p1', {
+        duration: 1500,
+        origin: 'right',
+        distance: '40px'
+
+    });
 }
 
 
